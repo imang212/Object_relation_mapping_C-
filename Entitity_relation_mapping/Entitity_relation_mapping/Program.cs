befc8f7993067f3caa832e7cb54ca86cacf5a217
Patrik Poklop <151776170+imang212@users.noreply.github.com>
2025-02-27 09:21:51 +0100

    Přidání prezentace

--- Entitity_relation_mapping/Entitity_relation_mapping/Program.cs
+++ Entitity_relation_mapping/Entitity_relation_mapping/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<MyDbContext>(options =>
+        {
+            
+        });
+        var app
+    }
+
+}
