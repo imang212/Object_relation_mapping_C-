5a3fbc627f5012b0f5b8d1d576f65cb88b4086ba
Furry NightShade <skyrelmchaos004@gmail.com>
2025-03-08 21:51:40 +0100

    QueryEngine

--- ER_WPF/ER_WPF/obj/Debug/net9.0-windows/ER_WPF.AssemblyInfo.cs
+++ ER_WPF/ER_WPF/obj/Debug/net9.0-windows/ER_WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ER_WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97db48cdd93bfd8c607f46239115fe51a5920b45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4733dc531a27e7e18efd937e75f7618a5938fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ER_WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ER_WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
